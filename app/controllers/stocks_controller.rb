73ef29bc30f4e0d2c5799185227760dede139ee2
fredlc <fredlefevre@outlook.com>
2017-12-16 15:03:13 -0500

    Refactored to handle errors with AJAX.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,19 +1,14 @@
 class StocksController < ApplicationController
 
   def search
-    if params[:stock].present?
-      @stock = Stock.new_from_lookup(params[:stock])
-      if @stock
-        respond_to do |format|
-          format.js { render partial: 'users/result' }
-        end
-      else
-        flash[:danger] = "Incorrect symbol."
-        redirect_to my_portfolio_path
-      end
+    if params[:stock].blank?
+      flash.now[:danger] = "You haven't entered anything in the search box."
     else
-      flash[:danger] = "You haven't entered anything in the search box."
-      redirect_to my_portfolio_path
+      @stock = Stock.new_from_lookup(params[:stock])
+      flash.now[:danger] = "Incorrect stock symbol." unless @stock
+    end
+    respond_to do |format|
+      format.js { render partial: 'users/result' }
     end
   end
 
