8a0906602cc42f1b0a4fb407b71e2b45fc1260f5
fredlc <fredlefevre@outlook.com>
2017-12-16 14:45:03 -0500

    Use AJAX to optimize stock search.

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,8 +1,20 @@
 class StocksController < ApplicationController
 
   def search
-    @stock = Stock.new_from_lookup(params[:stock])
-    render 'users/my_portfolio'
+    if params[:stock].present?
+      @stock = Stock.new_from_lookup(params[:stock])
+      if @stock
+        respond_to do |format|
+          format.js { render partial: 'users/result' }
+        end
+      else
+        flash[:danger] = "Incorrect symbol."
+        redirect_to my_portfolio_path
+      end
+    else
+      flash[:danger] = "You haven't entered anything in the search box."
+      redirect_to my_portfolio_path
+    end
   end
 
 end
